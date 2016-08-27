61ac981379a7e125eb84e1998e138f77478af6a2
mariano <mariano.grimaux@gmail.com>
2016-08-27 18:43:15 -0300

    Swing Menu.

--- ProyectoEclipse/src/GuiTemporalConsola.java
+++ ProyectoEclipse/src/GuiTemporalConsola.java
@@ -1,6 +1,11 @@
 
 import java.util.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
+
+import javax.swing.*;
+
 
 /// Simulacion para el modelo vista.-
 
@@ -16,68 +21,21 @@ public class GuiTemporalConsola {
 		GuiTemporalConsola main = new GuiTemporalConsola();
 		main.mostrarMenu();
 	}
-	public  void mostrarMenu() {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("MENU DE OPCIONES temporal.-");
-		System.out.println("-------------------------------------------------------");
-		System.out.println("0.- Salir");
-		System.out.println("1.- Crear Usuario");
-		System.out.println("2.- Quitar Usuario");
-		System.out.println("3.- Consultar tabla Puntos");
-		System.out.println("4.- Ver Usuarios");
-		System.out.println("5.- Jugar");
-		System.out.println("-------------------------------------------------------");
-		System.out.print("Opcion:");
-		try {
-			char s = (char) reader.read();
-			switch (s) {
-			case '1': {
-				crear();
-				break;
-			}
-			case '2': {
-		//		this.crearCliente();
-				break;
-			}
-			case '3': {
-		//		this.abrirCuenta();
-				break;
-			}
-			case '4': {
-				verUsuarios();
-				break;
-			}
-			case '5': {
-				//		this.abrirCuenta();
-				break;
-			}
-			case '0': {
-		//		this.salir();
-			}
-			}
-		} catch (Exception e) {
-		}
-	}
-	private void verUsuarios() {
-		
-			game.listarUsuarios();
-			mostrarMenu();
-	}
-	public void crear() {
-		try {
-			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-			System.out.println(" --- Agregar Usuario ---");
-			System.out.print("Ingrese nombre:");
-			String nombre = reader.readLine();
-			System.out.print("Ingrese email");
-			String email = reader.readLine();
-			System.out.println(" -----------------------");
-			// creo usuario
-			game.crearUsuario(nombre, email);
-			this.mostrarMenu();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
+
+	public void mostrarMenu() {
+		JFrame frame = new JFrame("Main menu");
+		frame.setSize(400, 700);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		frame.add(panel);
+		frame.setVisible(true);
+		JButton newUser = new JButton("Crear Usuario");
+		newUser.setActionCommand("newUser");
+		newUser.setBounds(10, 10, 150, 25);
+		panel.add(newUser);
+		JButton simon = new JButton("Simon dice");
+		simon.setBounds(200, 10, 150, 25);
+		panel.add(simon);
 	}
 }
