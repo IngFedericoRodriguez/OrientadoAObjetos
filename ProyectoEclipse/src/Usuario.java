aab81c43463d7353353c58dac954d39dcb1dde2c
mariano <mariano.grimaux@gmail.com>
2016-08-27 17:50:18 -0300

    tidy

--- ProyectoEclipse/src/Usuario.java
+++ ProyectoEclipse/src/Usuario.java
@@ -9,31 +9,37 @@ public class Usuario  {
 		this.nombre = nombre;
 		this.email = email;
 	}
+	
 	public String getNombre() {
 		return nombre;
 	}
+	
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
 	public float getScore() {
 		return score;
 	}
+	
 	public void setScore(float score) {
 		this.score = score;
 	}
-	public boolean soselUsuario(String clave)
-	{
+	
+	public boolean soselUsuario(String clave) {
 		return this.nombre.equals(clave);
 	}
+	
 	// Metodos de Negocio
 	public void sumarScore(float puntos){
 		this.score = puntos + this.score;
 	}
-	
 }
