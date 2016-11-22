c9e24c37c9e341f07ab2904cf543eb3d032da353
mariano <mariano.grimaux@gmail.com>
2016-11-22 10:22:11 -0300

    abstraer ventanaJuegoCartas

--- ProyectoEclipse/src/Vista/VentanaJuego.java
+++ ProyectoEclipse/src/Vista/VentanaJuego.java
@@ -22,44 +22,29 @@ import javax.swing.event.MenuListener;
 
 import Controlador.ControladorJuego;
 
-public class VentanaJuego extends JFrame implements ActionListener {
+abstract public class VentanaJuego extends JFrame implements ActionListener {
  
 	private static final long serialVersionUID = 1L;
-	private Marcador marcador;
-	private Tablero tablero;
-	private Timer myTimer;
-	private VistaCarta carta1;
-	private VistaCarta carta2;
-	private ControladorJuego controladorJuego;
-	private int state;
+	protected Marcador marcador;
+	protected Tablero tablero;
+	protected ControladorJuego controladorJuego;
 
 
-	
 	public VentanaJuego(Tablero tablero) {
 		super("PokeMemoria");
 		this.tablero = tablero;
 		this.setSize(800, 600);
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBaseLayout();
-		 myTimer = new Timer(250, this);
 	}
 	
 	public void setControladorJuego(ControladorJuego controladorJuego) {
 		this.controladorJuego = controladorJuego;
 	}
 	
-	private void setBaseLayout(){
-		GridLayout layout = new GridLayout(2,0);
-		layout.setHgap(5);
-		layout.setVgap(5);
-		getContentPane().setLayout(layout);
-	}
+	abstract protected void setBaseLayout();
 	
-	public void reset() {
-		tablero.removeAll();
-		marcador.removeAll();
-		getContentPane().removeAll();
-	}
+	abstract public void reset();
 	
 	public void agregarMarcador() {
 		marcador = new Marcador();
@@ -81,57 +66,13 @@ public class VentanaJuego extends JFrame implements ActionListener {
 		super.setVisible(flag);
 	}
 
-	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == myTimer) {
-			carta1.flip();
-			carta2.flip();
-			myTimer.stop();
-		} else {
-			VistaCarta vistaCarta = (VistaCarta) e.getSource();
-			if(!vistaCarta.isFlipped()){
-				switch (state) {
-					case 0:
-						carta1 = vistaCarta;
-						carta1.flip();
-						break;
-					case 1:
-						carta2 = vistaCarta;
-						carta2.flip();
-						break;
-				}
-				state++;
-			}
-			if (state == 2) {
-				boolean match = controladorJuego.match(carta1.getId(), carta2.getId());
-				if(match) {
-					carta2.lock();
-					carta1.lock();
-				} else {
-					myTimer.start();
-				}
-				state = 0;
-				updateSocre();
-				if (controladorJuego.nivelGanado()) {
-					controladorJuego.nuevoNivel(jugarNuevo());
-				}	
-			}
-		}
-    }
 	
-	private void updateSocre() {
+	protected void updateSocre() {
 		marcador.updatePuntos(Integer.toString(controladorJuego.getPuntos()));
 		marcador.updateAdivinadas(Integer.toString(controladorJuego.getAdivinadas()));
 		marcador.updateIntentos(Integer.toString(controladorJuego.getIntentos()));
 	}
 	
-	public void wait(int n){
-        long t0, t1;
-        t0 =  System.currentTimeMillis();
-        do{
-            t1 = System.currentTimeMillis();
-        }
-        while ((t1 - t0) < (n * 1000));
-    }
 	
 	public int jugarNuevo() {
 		int dialogButton = JOptionPane.YES_NO_OPTION;
---
+++ ProyectoEclipse/src/Vista/VentanaJuegoCartas.java
@@ -0,0 +1,79 @@
+package Vista;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+
+import javax.swing.Timer;
+
+public class VentanaJuegoCartas extends VentanaJuego{
+	private static final long serialVersionUID = 1L;
+	private VistaCarta carta1;
+	private VistaCarta carta2;
+	private int state;
+	private Timer myTimer;
+	
+	public VentanaJuegoCartas(Tablero tablero) {
+		super(tablero);
+		 myTimer = new Timer(250, this);
+	}
+
+	protected void setBaseLayout(){
+		GridLayout layout = new GridLayout(2,0);
+		layout.setHgap(5);
+		layout.setVgap(5);
+		getContentPane().setLayout(layout);
+	}
+	
+	public void reset() {
+		this.tablero.removeAll();
+		this.marcador.removeAll();
+		getContentPane().removeAll();
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == myTimer) {
+			carta1.flip();
+			carta2.flip();
+			myTimer.stop();
+		} else {
+			VistaCarta vistaCarta = (VistaCarta) e.getSource();
+			if(!vistaCarta.isFlipped()){
+				switch (state) {
+					case 0:
+						carta1 = vistaCarta;
+						carta1.flip();
+						break;
+					case 1:
+						carta2 = vistaCarta;
+						carta2.flip();
+						break;
+				}
+				state++;
+			}
+			if (state == 2) {
+				boolean match = controladorJuego.match(carta1.getId(), carta2.getId());
+				if(match) {
+					carta2.lock();
+					carta1.lock();
+				} else {
+					myTimer.start();
+				}
+				state = 0;
+				updateSocre();
+				if (controladorJuego.nivelGanado()) {
+					controladorJuego.nuevoNivel(jugarNuevo());
+				}	
+			}
+		}
+    }
+	
+	public void wait(int n){
+        long t0, t1;
+        t0 =  System.currentTimeMillis();
+        do{
+            t1 = System.currentTimeMillis();
+        }
+        while ((t1 - t0) < (n * 1000));
+    }
+
+}
